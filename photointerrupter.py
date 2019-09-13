5f9636663fff4125335ea8bc6cfc1ebc4a3602ed
Lily M Wielar <lwielar29@ccs.local>
2019-09-13 11:05:27 -0400

    added my photointerrupter code

---
+++ photointerrupter.py
@@ -0,0 +1,27 @@
+from digitalio import DigitalInOut, Direction, Pull
+import time
+import board
+
+interrupter = DigitalInOut(board.D7)
+interrupter.direction = Direction.INPUT
+interrupter.pull = Pull.UP
+
+counter = 0
+
+photo = False
+state = False
+
+max = 4
+start = time.time()
+while True:
+    photo = interrupter.value
+    if photo and not state:
+            counter += 1
+    state = photo
+
+    remaining = max - time.time()
+
+    if remaining <= 0:
+        print("Interrupts:", str(counter))
+        max = time.time() + 4
+        counter = 0
