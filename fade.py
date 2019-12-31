9ff273a371d9959500374d9fea201007073eaf9b
lwielar29 <55250640+lwielar29@users.noreply.github.com>
2019-12-31 11:56:18 -0500

    adding code comments

--- fade.py
+++ fade.py
@@ -1,14 +1,13 @@
-import time
-import board
-import pulseio
+import time # adding time libraries
+import board # adding board libraries
+import pulseio # adding pulseio libraries to enable fade similar to analog
 
-led = pulseio.PWMOut(board.D13, frequency=5000, duty_cycle=0)
+led = pulseio.PWMOut(board.D13, frequency=5000, duty_cycle=0)// naming led output and pin
 
-while True:
-    for i in range(100):
-        # PWM LED up and down
-        if i < 50:
+while True: 
+    for i in range(100):  # PWM LED up and down
+        if i < 50: #half the time
             led.duty_cycle = int(i * 2 * 65535 / 100)  # Up
-        else:
+        else: # the other half
             led.duty_cycle = 65535 - int((i - 50) * 2 * 65535 / 100)  # Down
-        time.sleep(0.03)
+        time.sleep(0.03) # wait for .3 seconds
