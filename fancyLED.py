cc9487333839e7896852d50e6e2b43fd4052b6d8
lwielar29 <55250640+lwielar29@users.noreply.github.com>
2019-12-31 12:31:17 -0500

    in progress

--- fancyLED.py
+++ fancyLED.py
@@ -1,18 +1,22 @@
+# Fancy LED Class
+# Lily Wielar
+# Class for fancy LED assignment that enables given code to alternate, blink, chase, and sparkle 6 leds
+
 import time
-from digitalio import DigitalInOut, Direction, Pull  #pylint: disable-msg=import-error
+from digitalio import DigitalInOut, Direction, Pull  #pylint: disable-msg=import-error ## from digitalio library import JUST DigitalInOut, Direction, Pull
 import random
 import board   #pylint: disable-msg=import-error
 
-class FancyLED:
-    def __init__ (self, pin1, pin2, pin3):
-        self.pin1 = DigitalInOut(pin1)
-        self.pin1.direction = Direction.OUTPUT
+class FancyLED: #class name is FancyLED
+    def __init__ (self, pin1, pin2, pin3): #automatically creates pin1, pin2, pin3 variables
+        self.pin1 = DigitalInOut(pin1) #pin1's pin will be "pin1"
+        self.pin1.direction = Direction.OUTPUT # pin1 is output
 
-        self.pin2 = DigitalInOut(pin2)
-        self.pin2.direction = Direction.OUTPUT
+        self.pin2 = DigitalInOut(pin2) #pin2's pin will be "pin1"
+        self.pin2.direction = Direction.OUTPUT # pin2 is output
 
-        self.pin3 = DigitalInOut(pin3)
-        self.pin3.direction = Direction.OUTPUT
+        self.pin3 = DigitalInOut(pin3) #pin3's pin will be "pin1"
+        self.pin3.direction = Direction.OUTPUT # pin3 is output
 
     def alternate(self):
         for i in range(0,5):
