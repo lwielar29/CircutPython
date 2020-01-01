ea465d54ac5d10b49262daee100392f2528841c3
lwielar29 <55250640+lwielar29@users.noreply.github.com>
2020-01-01 15:29:56 -0500

    commenting code

--- lcdpresses.py
+++ lcdpresses.py
@@ -1,27 +1,31 @@
-import time
+# LCD button presses
+# Lily Wielar
+# counts how many times a button was pressed and displays this number on an LCD screen
+
+import time 
 import board
 import digitalio
 from digitalio import DigitalInOut, Direction, Pull
-from lcd.lcd import LCD
-from lcd.i2c_pcf8574_interface import I2CPCF8574Interface
+from lcd.lcd import LCD # import necessary LCD libraries 
+from lcd.i2c_pcf8574_interface import I2CPCF8574Interface 
 from lcd.lcd import CursorMode
-lcd = LCD(I2CPCF8574Interface(0x3F), num_rows=2, num_cols=16)
+lcd = LCD(I2CPCF8574Interface(0x3F), num_rows=2, num_cols=16) # sets up LCD screen, 2 x 16
 
-redbutton = digitalio.DigitalInOut(board.D2)
-redbutton.direction = digitalio.Direction.INPUT
-redbutton.pull = digitalio.Pull.UP
+redbutton = digitalio.DigitalInOut(board.D2) #redbutton on pin D2
+redbutton.direction = digitalio.Direction.INPUT # button is on output
+redbutton.pull = digitalio.Pull.UP #pulling voltage direction
 
-lcd.print("pushes ")
-presses = 0
+lcd.print("pushes ") #prints the word pushes on LCD screen
+presses = 0 #variable "pushes" equals 0
 
-lcd.set_cursor_mode(CursorMode.LINE)
+lcd.set_cursor_mode(CursorMode.LINE) #moves the cursor down a line on LCD screen for future prints
 
 while True:
-    if redbutton.value:
-        lcd.set_cursor_pos(1, 4)
+    if redbutton.value: # if the redbutton isn't pressed...
+        lcd.set_cursor_pos(1, 4) #setting cursor position
         lcd.print("PRESSES")
-    else:
-        lcd.set_cursor_pos(1, 4)
-        presses += 1
-        lcd.print(presses)
+    else: # when the button is pressed...
+        lcd.set_cursor_pos(1, 4) #setting cursor position
+        presses += 1 # add 1 to the presses variable
+        lcd.print(presses) # prints number of presses
         time.sleep
