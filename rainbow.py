2b910d1e07dc4ccf19bd80930f48b89204e58a4c
lwielar29 <55250640+lwielar29@users.noreply.github.com>
2020-01-01 15:52:06 -0500

    commenting code

--- rainbow.py
+++ rainbow.py
@@ -1,36 +1,40 @@
-class RGB:
-    def__init__(self, red, green, blue):
-       self.red = pulseio.PWMOut(red,)
-       self.green = pulseio.PWMOut(green)
-       self.blue = pulseio.PWMOut(blue)
+#Rainbow
+# Lily Wielar
+# creating class RGB to run with assigned code that makes three RGB leds flash different colors
+
+class RGB: #class name RGB
+    def__init__(self, red, green, blue): #automatically creates functions red, green, blue
+       self.red = pulseio.PWMOut(red)  # red will pulseio PWM with pin "red"
+       self.green = pulseio.PWMOut(green) # green will pulseio PWM with pin "green"
+       self.blue = pulseio.PWMOut(blue) # blue will pulseio PWM with pin "blue"
 
 
-def red (self):
-red.duty_cycle = 0
-green.duty_cycle = 0
-blue.duty_cycle = 65535
+def red (self): # function red...
+red.duty_cycle = 65535 # 100% red
+green.duty_cycle = 0   # 0% green
+blue.duty_cycle = 0  # 0% blue
 
-def blue (self):
-red.duty_cycle = 0
-green.duty_cycle = 0
-blue.duty_cycle = 65535
+def blue (self): # function blue...
+red.duty_cycle = 0 # 0% red
+green.duty_cycle = 0   # 0% green
+blue.duty_cycle = 65535   # 100% blue
 
-def green (self):
-red.duty_cycle = 0
-green.duty_cycle = 65535
-blue.duty_cycle = 0
+def green (self): # function green...
+red.duty_cycle = 0 # 0% red
+green.duty_cycle = 65535 # 100% green
+blue.duty_cycle = 0  # 100% blue
 
-def cyan(self):
+def cyan(self): # function cyan...
 red.duty_cycle = 0   # 0% red
 green.duty_cycle = 65535    # 100% green
-blue.duty_cycle = 65535
+blue.duty_cycle = 65535  # 100% blue
 
-def yellow(self):
+def yellow(self): # function yellow...
 red.duty_cycle = 65535   # 100% red
 green.duty_cycle = 65535     # 100% green
-blue.duty_cycle = 0
+blue.duty_cycle = 0    #0% blue
 
-def magenta(self):
+def magenta(self): # function magenta...
 red.duty_cycle = 65535   # 100% red
 green.duty_cycle = 0     # 0% green
 blue.duty_cycle = 65535  # 100% blue
