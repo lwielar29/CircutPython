cb4e6520a5923677cc67216e5d514fc244fc59e6
lwielar29 <55250640+lwielar29@users.noreply.github.com>
2020-01-01 14:39:08 -0500

    commenting code

--- hello_vs_code.py
+++ hello_vs_code.py
@@ -1,5 +1,9 @@
-import time
+#Hello VS Code
+#Lily Wielar
+# praticing using the platform VS code to write code, upload code, and send to github - wrote "hello" on serial monitor 
+
+import time # importing time libraries
 
 while True:
-    print("hello")
-    time.sleep(.1)
+    print("hello") #prints the word "hello" on serial monitor
+    time.sleep(.1) #pause
