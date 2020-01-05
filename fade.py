6d3001b020b9517e2c777c042297fd48f14b45e2
lwielar29 <55250640+lwielar29@users.noreply.github.com>
2020-01-05 11:26:00 -0500

    fixing comment

--- fade.py
+++ fade.py
@@ -2,7 +2,7 @@ import time # adding time libraries
 import board # adding board libraries
 import pulseio # adding pulseio libraries to enable fade similar to analog
 
-led = pulseio.PWMOut(board.D13, frequency=5000, duty_cycle=0)// naming led output and pin
+led = pulseio.PWMOut(board.D13, frequency=5000, duty_cycle=0) #naming led output, pin, frequency and duty cycle of pulse(PWM)
 
 while True: 
     for i in range(100):  # PWM LED up and down
